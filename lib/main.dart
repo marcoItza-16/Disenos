bfb4c9bd96d602c7281de6b5f4ea5a0a9a528488
marcoestrada-17 <javier.itza.est@qroo.nuevaescuela.mx>
2022-03-04 00:16:21 -0500

    Merge branch

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: 'botones',
+      initialRoute: 'basico',
       routes: {
         'basico'  :  (BuildContext context) => BasicoPage(),
         'scroll'  :  (BuildContext context) => ScrollPage(),
