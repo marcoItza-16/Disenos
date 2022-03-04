3862ab4a25e128a6aa967a0325c32887889ac866
marcoestrada-17 <javier.itza.est@qroo.nuevaescuela.mx>
2022-03-04 00:34:26 -0500

    Diseño Compuesto -Fondo y Gradiente

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:disenos/src/pages/botones_page.dart';
 
 //Diseños con Scroll 5
 //Merge remote-tracking branch
+//Diseño Compuesto -Fondo y Gradiente
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
